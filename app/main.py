b4a230a4356009875c134a0fc8f38b5c0897c395
Marcin Paliwoda <paliwoda.marcin@zoho.com>
2024-06-06 12:27:34 +0200

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -1,18 +1,57 @@
-import socket
+import asyncio
+from typing import TypeAlias
+
+Address: TypeAlias = str
+Port: TypeAlias = int
+
+ClientId: TypeAlias = str
+ClientTask: TypeAlias = asyncio.Task
+
+_clients: dict[ClientId, ClientTask] = {}
 
 
-def main() -> None:
-    server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
-    client, _addr = server_socket.accept() # wait for client
+async def on_client_connect(reader: asyncio.StreamReader, writer: asyncio.StreamWriter):
+    client_id = writer.get_extra_info('peername')
 
+    def client_cleanup(future: asyncio.Future):
+        try:
+            future.result()
+        except Exception as e:
+            print(e)
+        finally:
+            del _clients[client_id]
+
+    client_task= asyncio.ensure_future(listen_task(reader, writer))
+    client_task.add_done_callback(client_cleanup)
+
+    _clients[client_id] = client_task
+
+
+async def listen_task(reader: asyncio.StreamReader, writer: asyncio.StreamWriter):
     while True:
-        message = client.recv(1024)
+        data = await reader.read(1024)
 
-        if not message:
+        if data == b"":
             break
 
-        client.send("+PONG\r\n".encode())
+        writer.write("+PONG\r\n".encode())
+        await writer.drain()
+
+
+async def main():
+    server_socket = await asyncio.start_server(
+        on_client_connect,
+        host="127.0.0.1",
+        port=6379,
+        reuse_port=True,
+        start_serving=False,
+    )
+
+    await server_socket.serve_forever()
+
+    server_socket.close()
+    await server_socket.wait_closed()
 
 
 if __name__ == "__main__":
-    main()
+    asyncio.run(main())
