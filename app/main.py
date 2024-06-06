68bab32d3fd4dcdf1a4e64ca927a8a14b44c2c3a
Marcin Paliwoda <paliwoda.marcin@zoho.com>
2024-06-06 11:54:10 +0200

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -3,7 +3,14 @@ import socket
 
 def main() -> None:
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
-    server_socket.accept() # wait for client
+    client, _addr = server_socket.accept() # wait for client
+
+    raw_message = client.recv(1024)
+    _message = raw_message.decode()
+
+    resp = "+PONG\r\n"
+
+    client.send(resp.encode())
 
 
 if __name__ == "__main__":
